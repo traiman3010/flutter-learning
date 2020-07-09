e567373e3b7d20922317d47ba0134d26131bd44d
traiman <goolemzaa007@gmail.com>
2020-07-09 19:22:14 +0700

    End EP6

--- lib/main.dart
+++ lib/main.dart
@@ -3,43 +3,23 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 
-class MyApp extends StatelessWidget 
-{
+class MyApp extends StatelessWidget {
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Flutter My_App"),
-          leading: IconButton(
-            icon: Icon(
-              Icons.menu,
-              color: Colors.white,
-          ),
-          tooltip: 'Menu',
-          onPressed: null,
+          title: Text("Flutter App"),
         ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.search,
-              color: Colors.white
-            ),
-            tooltip: 'Search Data',
-            onPressed: null,
-          ),
-        ],
-      ),  /// Blue tap
-  
-      body: Container(
-        alignment: Alignment.center,
-        child: Text("Hello World"),
-      ),  /// White tap
-      
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.thumb_up),
-        onPressed: () => {},
-      ), /// Action tap
-    ));
+        body: Container(
+          alignment: Alignment.center, /// Position
+          child: Text("Hello Worlds"), /// text
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.thumb_up), /// IconBlueFrame
+          onPressed: () => {}, /// Press 
+        ),
+      )
+    );
   }
 }
 
